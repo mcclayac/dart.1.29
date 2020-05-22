0c560c9e83bd4d8727fa7be6e021eab9a7b6e2e6
mcclayac <anthony.mcclay@gmail.com>
2020-05-22 14:51:39 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,14 @@
+//code 1.29
+main(List<String> arguments) {
+  var myProducts = {
+    'first' : 'TV',
+    'second' : 'Refrigerator',
+    'third' : 'Mobile',
+    'fourth' : 'Tablet',
+    'fifth' : 'Computer'
+  };
+  print(myProducts['third']);
+
+  print(myProducts.toString());
+  print(myProducts['fourth']);
+}
